9668e9f81463198d17d97b60c554ed0196ebccd4
Rama Chandu <rama.chandu57@gmail.com>
2020-11-10 18:15:18 +0530

    Removed some code

--- play_testing/src/greeter/__tests__/index.test.ts
+++ play_testing/src/greeter/__tests__/index.test.ts
@@ -24,48 +24,18 @@ describe("Greeter", () => {
     //   expect(greeterInstance.sayHi("Rama", 1)).toBe("Hello Mr.Rama!");
     // });
 
-    // it("Should greet with a gender prefix if provided - female", () => {
-    //   expect(greeterInstance.sayHi("Ramya", 2)).toBe("Hello Mrs.Ramya!");
-    // });
+    // // it("Should greet with a gender prefix if provided - female", () => {
+    // //   expect(greeterInstance.sayHi("Ramya", 2)).toBe("Hello Mrs.Ramya!");
+    // // });
 
-    // it("Should greet with a gender prefix if gender provided as string", () => {
-    //   expect(greeterInstance.sayHi("Rama", 'm')).toBe("Hello Mr.Rama!");
-    // });
+    // // it("Should greet with a gender prefix if gender provided as string", () => {
+    // //   expect(greeterInstance.sayHi("Rama", 'm')).toBe("Hello Mr.Rama!");
+    // // });
 
-    // it("Should greet with a gender prefix if gender provided as string - female", () => {
-    //   expect(greeterInstance.sayHi("Ramya", 'f')).toBe("Hello Mrs.Ramya!");
-    // });
+    // // it("Should greet with a gender prefix if gender provided as string - female", () => {
+    // //   expect(greeterInstance.sayHi("Ramya", 'f')).toBe("Hello Mrs.Ramya!");
+    // // });
   });
-
-  // describe("sayBye", () => {
-  //   beforeEach(() => {
-  //     greeterInstance = new Greeter();
-  //   });
-
-  //   it("should greet as guest if no name is given", () => {
-  //     expect(greeterInstance.sayBye()).toBe("Hello Guest!");
-  //   });
-
-  //   it("Should greet by the name entered", () => {
-  //     expect(greeterInstance.sayBye("Rama")).toBe("Hello Rama!");
-  //   });
-
-  //   it("Should greet with a gender prefix if provided", () => {
-  //     expect(greeterInstance.sayBye("Rama", 1)).toBe("Hello Mr.Rama!");
-  //   });
-
-  //   it("Should greet with a gender prefix if provided - female", () => {
-  //     expect(greeterInstance.sayBye("Ramya", 2)).toBe("Hello Mrs.Ramya!");
-  //   });
-
-  //   it("Should greet with a gender prefix if gender provided as string", () => {
-  //     expect(greeterInstance.sayBye("Rama", 'm')).toBe("Hello Mr.Rama!");
-  //   });
-
-  //   it("Should greet with a gender prefix if gender provided as string - female", () => {
-  //     expect(greeterInstance.sayBye("Ramya", 'f')).toBe("Hello Mrs.Ramya!");
-  //   });
-  // });
 
   // describe("sayBye", () => {
   //   beforeEach(() => {
